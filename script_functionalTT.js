ea9523efcfdb2540424573f23a212dafb348c359
Will Luckman <wmluckman@gmail.com>
2023-11-19 14:24:16 -0500

    static, functional

--- script_functionalTT.js
+++ script_functionalTT.js
@@ -58,26 +58,20 @@ svg3.append("rect")
 
 
 // SVG 4
-const svg4Container = d3.select("#svg-container-4");
-const svg4 = svg4Container
+const svg4 = d3.select("#svg-container-4")
   .append("svg")
   .attr("width", "100%")
-  .attr("height", 1250000)
+  .attr("height", 1250000 + margin.top + margin.bottom)
   .append("g")
   .attr("transform", `translate(${margin.left},${margin.top})`);
 
-// Create a 1,000,000,000 pixel rectangle
+// Create a 1,250,000 pixel rectangle
 svg4.append("rect")
   .attr("width", 800)
   .attr("height", 1250000)
   .attr("x", 0)
   .attr("y", 0)
   .attr("fill", "black");
-
-// Create a separate container for tooltips and lines
-const tooltipContainer = svg4Container
-  .append("div")
-  .attr("class", "tooltip-container");
 
 // Load your dataset
 d3.csv("data/BigNumbers_Clean.csv", d => d).then(data => {
@@ -97,24 +91,24 @@ d3.csv("data/BigNumbers_Clean.csv", d => d).then(data => {
   const tooltipMargin = 10; // Adjust as needed
 
   data.forEach(d => {
-    const xPosition = d.Number % 800;
-    const yPosition = Math.floor(d.Number / 800);
+    const xPosition = d.Number % 800 + margin.left + tooltipMargin;
+    const yPosition = Math.floor(d.Number / 800) + margin.top + tooltipMargin;
 
     // Create a line from the pixel/rectangle to the tooltip
     svg4.append("line")
       .attr("class", "tooltip-line")
-      .attr("x1", xPosition)
-      .attr("y1", yPosition)
+      .attr("x1", d.Number % 800)
+      .attr("y1", Math.floor(d.Number / 800))
       .attr("x2", xPosition)
       .attr("y2", yPosition)
       .attr("stroke", "white");
 
-    // Append foreignObject for the dynamic tooltip to the tooltipContainer
-    const foreignObject = tooltipContainer.append("div")
+    // Append foreignObject for the dynamic tooltip
+    const foreignObject = svg4.append("foreignObject")
       .attr("class", "dynamic-tooltip")
-      .style("position", "absolute")
-      .style("left", `${svg4Container.node().getBoundingClientRect().left + xPosition + margin.left + tooltipMargin}px`)
-      .style("top", `${svg4Container.node().getBoundingClientRect().top + yPosition + margin.top + tooltipMargin + window.scrollY}px`);
+      .attr("x", xPosition)
+      .attr("y", yPosition)
+      .attr("width", 200) // Set a fixed width, adjust as needed;
 
     // Append xHTML content to the foreignObject
     const tooltipDiv = foreignObject.append("xhtml:div")
@@ -131,6 +125,6 @@ d3.csv("data/BigNumbers_Clean.csv", d => d).then(data => {
 
     // Calculate and set height based on content
     const tooltipHeight = tooltipDiv.node().getBoundingClientRect().height;
-    foreignObject.style("height", `${tooltipHeight}px`);
+    foreignObject.attr("height", tooltipHeight);
   });
 });
